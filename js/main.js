00eda92db7c4f51b31714f6da9a196fb7070a029
Dadit <dadit.utama@gmail.com>
2024-09-23 15:42:00 +0700

    WebGL (feat): defining shader programs vertex and fragment

--- js/main.js
+++ js/main.js
@@ -98,18 +98,3 @@ function draw() {
 }
 
 //draw();
-
-function main() {
-  const canvas = document.getElementById("canvas");
-  const gl = canvas.getContext("webgl");
-
-  if (gl === null) {
-    alert("WebGL not supported");
-    return;
-  }
-
-  gl.clearColor(0.0, 0.0, 0.0, 1.0);
-  gl.clear(gl.COLOR_BUFFER_BIT);
-}
-
-main();
